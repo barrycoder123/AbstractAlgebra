22dea2cf0b9a6c2b8c0113cefa8364e0bfa0346e
Ibrahima Barry <ibarry01@tufts.edu>
2020-11-11 00:04:08 -0500

    new folder

---
+++ AlgebraicCodingTheory/linear_code.py
@@ -0,0 +1,10 @@
+'''
+Write a program to implement a (16, 12)-linear code. Your program should be able to
+encode and decode messages using coset decoding. Once your program is written, write
+a program to simulate a binary symmetric channel with transmission noise. Compare
+the results of your simulation with the theoretically predicted error probability.
+
+Reference: "Abstract Algebra Theory and Applications by Thomas W. Judson 
+'''
+
+
